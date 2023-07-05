b2b33d31c859e73e16efdbf3eae0e10dcbe41910
Wesley Maia <72634262+WMaia9@users.noreply.github.com>
2023-07-05 23:27:19 +0000

    new

---
+++ 5.3.py
@@ -0,0 +1,44 @@
+class Aluno:
+    def __init__(self, nome, idade, sexo):
+        self.nome = nome
+        self.idade = idade
+        self.sexo = sexo
+
+
+class Pilha:
+    def __init__(self, tamanho_maximo):
+        self.tamanho_maximo = tamanho_maximo
+        self.pilha = []
+
+    def push(self, aluno):
+        if len(self.pilha) < self.tamanho_maximo:
+            self.pilha.append(aluno)
+
+    def pop(self):
+        if len(self.pilha) > 0:
+            return self.pilha.pop()
+
+    def imprimir_pilha(self):
+        while len(self.pilha) > 0:
+            aluno = self.pop()
+            print(f"{aluno.nome} {aluno.idade} {aluno.sexo}", end=" ")
+
+
+tamanho = int(input())
+pilha = Pilha(tamanho)
+
+entrada = input().split()
+
+for i in range(0, len(entrada), 4):
+    operacao = entrada[i]
+    nome = entrada[i + 1]
+    idade = entrada[i + 2]
+    sexo = entrada[i + 3]
+
+    if operacao == 'i':
+        aluno = Aluno(nome, idade, sexo)
+        pilha.push(aluno)
+    elif operacao == 'r':
+        pilha.pop()
+
+pilha.imprimir_pilha()
